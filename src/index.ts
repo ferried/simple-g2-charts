1315713764e4cfc052f2ef91e5d8c28c6e600a6f
ferried <harlancui@outlook.com>
2020-12-04 15:19:35 +0800

    :bug: (yzlongareachart): fix import bug

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@
  * @Author: ferried
  * @Email: harlancui@outlook.com
  * @Date: 2020-11-30 09:13:47
- * @LastEditTime: 2020-11-30 15:10:53
+ * @LastEditTime: 2020-12-04 15:19:08
  * @LastEditors: ferried
  * @Description: Basic description
  * @FilePath: /simple-g2-charts/src/index.ts
  * @LICENSE: Apache-2.0
  */
 import YzAreaChart from "./YzAreaChart/YzAreaChart"
+import YzLongAreaChart from "./YzLongAreaChart/YzLongAreaChart"
 
 export { YzAreaChart }
+export { YzLongAreaChart }
